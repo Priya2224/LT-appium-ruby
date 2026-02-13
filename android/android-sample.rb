900c26953d14c3b0f6b4c7ef8e52ad2e3781603c
Priya2224 <138658561+Priya2224@users.noreply.github.com>
2026-02-13 13:20:13 +0530

    Update device name

--- android/android-sample.rb
+++ android/android-sample.rb
@@ -6,9 +6,9 @@ accessToken= ENV["LT_ACCESS_KEY"] || "LT_AccessKey" # Enter your Access Key here
 
 caps = {
     "lt:options" => {
-        :deviceName => "Pixel 6",
+        :deviceName => "Galaxy S24",
         :platformName => "Android",
-        :platformVersion => "13",
+        :platformVersion => "14",
         :build => "Ruby Vanilla - Android",
         :name => "Ruby Android Test",
         :isRealMobile => true,
